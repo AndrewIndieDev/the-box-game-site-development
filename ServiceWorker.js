1f27095f671c8a949406913224d5492920522214
PresDev <andrewindiedev@gmail.com>
2025-05-18 16:13:23 +1000

    Fixed some networking issues

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Andrew Dowsett-Carrot Commotion-0.4.1";
+const cacheName = "Andrew Dowsett-Carrot Commotion-0.4.2";
 const contentToCache = [
     "Build/the-box-game-web.loader.js",
     "Build/the-box-game-web.framework.js.unityweb",
